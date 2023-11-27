ca24b418b70778d33378271547150f9917f0e086
Curtis Tarr <30830713+CurtisTarr@users.noreply.github.com>
2023-11-27 20:37:11 +0000

    adding link to github repo

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,19 @@ import Diff from './components/Diff.vue'
 </script>
 
 <template>
+  <a href="https://github.com/CurtisTarr/diff" target="_blank">
+    <img src="./assets/github-mark-white.svg" alt="GitHub logo" class="github-logo"/>
+  </a>
   <h2>Diff</h2>
   <Diff />
 </template>
+
+<style scoped>
+.github-logo {
+  position: fixed;
+  top: 10px;
+  left: 10px;
+  width: 30px;
+  height: 30px;
+}
+</style>
